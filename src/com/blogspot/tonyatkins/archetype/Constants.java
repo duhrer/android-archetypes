3bef27302230c332580be03b7279c6497ce4a9bf
Tony Atkins <duhrer@gmail.com>
2013-01-18 22:04:58 +0100

    Tested sample sound recording activity (which will be converted to a standalone library).

--- src/com/blogspot/tonyatkins/archetype/Constants.java
+++ src/com/blogspot/tonyatkins/archetype/Constants.java
@@ -2,4 +2,8 @@ package com.blogspot.tonyatkins.archetype;
 
 public class Constants {
 	public static String TAG = "Archetype";
+	public static final String HOME_DIRECTORY = "/sdcard/com.blogspot.tonyatkins.archetype";
+	public static final String IMAGE_DIRECTORY = HOME_DIRECTORY + "/images";
+	public static final String SOUND_DIRECTORY = HOME_DIRECTORY + "/sounds";
+
 }
